f8f7fe9181a70762a86cfd65eebef645c7a017af
AlexandraMihalevschi <68810700+AlexandraMihalevschi@users.noreply.github.com>
2020-11-10 19:38:06 +0300

    Add files via upload

---
+++ Problema-1.py
@@ -0,0 +1,10 @@
+n = eval(input("Dați numarul de zile a lunii: "))
+if n==28:
+    print("februarie(an obișnuit)")
+elif n==29:
+    print("februarie(an obișnuit)")
+elif n==30:
+    print("aprilie, iunie, septembrie, noiembrie")
+elif n==31:
+    print("ianuarie, martie, mai, iulie, august, octombrie, decembrie")
+else: print("Nu exista luni cu asa numar de zile")
